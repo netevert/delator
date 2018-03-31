e8ea2bb37324ccc0be598db8cd053fee8a58c00e
netevert <netevert@protonmail.com>
2018-04-01 00:53:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+DELATOR
+=======
+[![baby-gopher](https://raw.githubusercontent.com/drnic/babygopher-site/gh-pages/images/babygopher-logo-small.png)](http://www.babygopher.org)
+
+Delator (*lat.* **informer**) is a tool to perform subdomain enumeration through abusing certificate transparency logs. It expands on the original work done by [Sheila A. Berta](https://github.com/UnaPibaGeek) with her [CTFR](https://github.com/UnaPibaGeek/ctfr) tool and leverages the speed and power of [Go](https://golang.org/). Delator is work in progress and V.1 is due for release **very** soon ... so do make sure to check back frequently.
---
+++ delator.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"github.com/fatih/color"
+	"golang.org/x/net/publicsuffix"
+	"regexp"
+)
+
+var (
+	g = color.New(color.FgHiGreen)
+	y = color.New(color.FgYellow)
+	r = color.New(color.FgHiRed)
+	domain = flag.String("d", "", "target domain")
+	version = flag.Bool("v", false, "display version")
+	utilDescription = "delator -d domain"
+	banner = `
+8"""8 8""" 8    8"""8 ""8"" 8""88 8""8  
+8e  8 8eee 8e   8eee8   8e  8   8 8ee8e
+88  8 88   88   88  8   88  8   8 88  8
+88ee8 88ee 88ee 88  8   88  8eee8 88  8`
+	appVersion = "1.0.0"
+)
+
+func setup(){
+	
+	flag.Usage = func() {
+		g.Printf(banner)
+		// fmt.Printf(" v.%s\n", version)
+		y.Printf("\nwritten & maintained with ♥ by NetEvert\n")
+		// fmt.Printf("[v.%s]\n\n", version)
+		fmt.Println(utilDescription)
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *version {
+		y.Printf("DELATOR")
+		fmt.Printf(" v.%s\n", appVersion)
+		os.Exit(1)
+	}
+
+	if *domain == "" {
+		r.Printf("\nplease supply a domain\n\n")
+		fmt.Println(utilDescription)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
+
+// validates domains using regex
+func validateDomainName(domain string) bool {
+
+	patternStr := `^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$`
+
+	RegExp := regexp.MustCompile(patternStr)
+	return RegExp.MatchString(domain)
+}
+
+// sanitizes domains inputted into dnsmorph
+func processInput(input string) (sanitizedDomain string) {
+	if !validateDomainName(input) {
+		r.Printf("\nplease supply a valid domain\n\n")
+		fmt.Println(utilDescription)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	sanitizedDomain, _ = publicsuffix.EffectiveTLDPlusOne(input)
+	return sanitizedDomain
+}
+
+func main(){
+	setup()
+	sanitizedDomain := processInput(*domain)
+	fmt.Println(sanitizedDomain)
+}

b4db1b60fac5bc09fe4c099f5ede5453bb6ac87d
netevert <netevert@protonmail.com>
2018-04-01 23:34:30 +0100

    first version

--- delator.go
+++ delator.go
@@ -1,31 +1,76 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"os"
 	"github.com/fatih/color"
 	"golang.org/x/net/publicsuffix"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"regexp"
+	"strings"
+	"time"
 )
 
 var (
-	g = color.New(color.FgHiGreen)
-	y = color.New(color.FgYellow)
-	r = color.New(color.FgHiRed)
-	domain = flag.String("d", "", "target domain")
-	version = flag.Bool("v", false, "display version")
+	g               = color.New(color.FgHiGreen)
+	y               = color.New(color.FgHiYellow)
+	r               = color.New(color.FgHiRed)
+	domain          = flag.String("d", "", "target domain")
+	version         = flag.Bool("v", false, "display version")
 	utilDescription = "delator -d domain"
-	banner = `
+	myClient        = &http.Client{Timeout: 10 * time.Second}
+	appVersion      = "1.0.0"
+	banner          = `
 8"""8 8""" 8    8"""8 ""8"" 8""88 8""8  
 8e  8 8eee 8e   8eee8   8e  8   8 8ee8e
 88  8 88   88   88  8   88  8   8 88  8
 88ee8 88ee 88ee 88  8   88  8eee8 88  8`
-	appVersion = "1.0.0"
 )
 
-func setup(){
-	
+type Data struct {
+	Issuer_ca_id        int    `json:"issuer_ca_id"`
+	Issuer_name         string `json:"issuer_name"`
+	Name_value          string `json:"name_value"`
+	Min_cert_id         int    `json:"min_cert_id"`
+	Min_entry_timestamp string `json:"min_entry_timestamp"`
+	Not_after           string `json:"not_after"`
+	Not_before          string `json:"not_before"`
+}
+
+// fetches certificate transparency json data
+func fetchData(url string) ([]Data, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	result := strings.Replace(string(body), "}{", "},{", -1)
+	d := fmt.Sprintf("[%s]", result)
+
+	keys := make([]Data, 0)
+	json.Unmarshal([]byte(d), &keys)
+	return keys, err
+}
+
+// deduplicates and prints subdomains
+func printData(data []Data) {
+	counter := make(map[string]int)
+	for _, i := range data {
+		counter[i.Name_value]++
+		if counter[i.Name_value] == 1 {
+			y.Println(i.Name_value)
+		}
+	}
+}
+
+// sets up command-line arguments and default responses
+func setup() {
 	flag.Usage = func() {
 		g.Printf(banner)
 		y.Printf("\nwritten & maintained with ♥ by NetEvert\n\n")
@@ -67,11 +112,16 @@ func processInput(input string) (sanitizedDomain string) {
 		os.Exit(1)
 	}
 	sanitizedDomain, _ = publicsuffix.EffectiveTLDPlusOne(input)
-	return sanitizedDomain
+	return "%." + sanitizedDomain
 }
 
-func main(){
+// main program entry point
+func main() {
 	setup()
 	sanitizedDomain := processInput(*domain)
-	fmt.Println(sanitizedDomain)
+	keys, err := fetchData(fmt.Sprintf("https://crt.sh/?q=%s&output=json", sanitizedDomain))
+	if err != nil {
+		panic(err.Error())
+	}
+	printData(keys)
 }
